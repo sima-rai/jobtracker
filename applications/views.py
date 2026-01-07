f4eb711272c6c073429297e5aee53f20da2bf392
seema <raiseema760@gmail.com>
2026-01-07 12:44:01 +0000

    User signup and login

--- applications/views.py
+++ applications/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 
@@ -12,3 +14,46 @@ def hello_world(self):
 
 def home(request):
     return render(request, 'applications/home.html')
+
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+            return redirect("signup")
+        
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+        login(request, user)
+        return redirect("home")
+    return render(request, 'applications/signup.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid credentials")
+            return redirect("login")
+        
+    return render(request, 'applications/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
